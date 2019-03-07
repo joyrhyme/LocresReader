2cac25f16602b2c69d362c3d1eef09bd8cd35d58
SGKoishi <sgkoishi@hotmail.com>
2019-03-07 10:00:49 -0800

    Add code.

--- LocresReader/Program.cs
+++ LocresReader/Program.cs
@@ -11,7 +11,7 @@ namespace LocresReader
     {
         static void Main(string[] args)
         {
-            var dict = new SortedDictionary<string, SortedDictionary<string, string>>();
+            var dict = new SortedDictionary<string, SortedDictionary<string, SortedDictionary<string, string>>>();
             string readString(BinaryReader br)
             {
                 var length = br.ReadInt32();
@@ -30,37 +30,83 @@ namespace LocresReader
                     return Encoding.Unicode.GetString(data);
                 }
             }
-            var reader = new BinaryReader(new MemoryStream(File.ReadAllBytes(args[0])));
-            var MagicNumber = reader.ReadBytes(16);
-            var VersionNumber = reader.ReadByte();
-            var LocalizedStringArrayOffset = reader.ReadInt64();
-            var CurrentFileOffset = reader.BaseStream.Position;
-            reader.BaseStream.Position = LocalizedStringArrayOffset;
-            var arrayLength = reader.ReadInt32();
-            var LocalizedStringArray = new string[arrayLength];
-            for (var i = 0; i < arrayLength; i++)
+            foreach (var lang in new[] { /* "de", "en", "es", "fr", "it", "ja", "ko", "pl", "pt-BR", "ru", "th", "zh-Hans", "zh-Hant" */ "en", "zh-Hans" })
             {
-                LocalizedStringArray[i] = readString(reader);
-            }
-            reader.BaseStream.Position = CurrentFileOffset;
-            var NamespaceCount = reader.ReadUInt32();
-            for (var i = 0; i < NamespaceCount; i++)
-            {
-                var Namespace = readString(reader);
-                if (!dict.ContainsKey(Namespace))
+                var reader = new BinaryReader(new MemoryStream(File.ReadAllBytes($@"E:\Projects\dbdt\dbd\DeadByDaylight\Content\Localization\Game\{lang}\DeadByDaylight.locres")));
+                var MagicNumber = reader.ReadBytes(16);
+                var VersionNumber = reader.ReadByte();
+                var LocalizedStringArrayOffset = reader.ReadInt64();
+                var CurrentFileOffset = reader.BaseStream.Position;
+                reader.BaseStream.Position = LocalizedStringArrayOffset;
+                var arrayLength = reader.ReadInt32();
+                var LocalizedStringArray = new string[arrayLength];
+                for (var i = 0; i < arrayLength; i++)
                 {
-                    dict[Namespace] = new SortedDictionary<string, string>();
+                    LocalizedStringArray[i] = readString(reader);
                 }
-                var KeyCount = reader.ReadInt32();
-                for (var j = 0; j < KeyCount; j++)
+                reader.BaseStream.Position = CurrentFileOffset;
+                var NamespaceCount = reader.ReadUInt32();
+                for (var i = 0; i < NamespaceCount; i++)
                 {
-                    var Key = readString(reader);
-                    var _SourceStringHash = BitConverter.ToString(reader.ReadBytes(4)).Replace("-", ""); // String after CRC slice by 8
-                    var LocalizedStringIndex = reader.ReadInt32();
-                    dict[Namespace][Key] = LocalizedStringArray[LocalizedStringIndex];
+                    var Namespace = readString(reader);
+                    if (!dict.ContainsKey(Namespace))
+                    {
+                        dict[Namespace] = new SortedDictionary<string, SortedDictionary<string, string>>();
+                    }
+                    var KeyCount = reader.ReadInt32();
+                    for (var j = 0; j < KeyCount; j++)
+                    {
+                        var Key = readString(reader);
+                        if (!dict[Namespace].ContainsKey(Key))
+                        {
+                            dict[Namespace][Key] = new SortedDictionary<string, string>();
+                        }
+                        dict[Namespace][Key]["SourceStringHash"] = BitConverter.ToString(reader.ReadBytes(4)).Replace("-", "");
+                        var LocalizedStringIndex = reader.ReadInt32();
+                        dict[Namespace][Key][lang] = LocalizedStringArray[LocalizedStringIndex];
+                    }
                 }
+                #region Legacy
+                /*
+                var data = File.ReadAllBytes($@"E:\Projects\dbdt\dbd\DeadByDaylight\Content\Localization\Game\{lang}\DeadByDaylight.locres");
+                var dataStart = BitConverter.ToInt32(data, 17);
+                var count = BitConverter.ToInt32(data, dataStart);
+                Console.WriteLine($"{lang} has {count} items.");
+                var keysData = new byte[dataStart - 0x29];
+                Buffer.BlockCopy(data, 0x29, keysData, 0, keysData.Length);
+                dataStart += 4;
+                var valueData = new byte[data.Length - dataStart];
+                Buffer.BlockCopy(data, dataStart, valueData, 0, valueData.Length);
+                var kbr = new BinaryReader(new MemoryStream(keysData));
+                var vbr = new BinaryReader(new MemoryStream(valueData));
+                for (var i = 0; i < count; i++)
+                {
+                    var str = Encoding.Unicode.GetString(kbr.ReadBytes(64)); // 一个Unicode的32位十六进制字符串，貌似是Hash
+                    kbr.ReadBytes(2); // 总是00 00
+                    var key = kbr.ReadInt32(); // 无规律的疑似随机数，和Hash相关（Hash相同时这个值相同）
+                    var unknown = kbr.ReadInt32(); // 从0开始 貌似是按顺序增加
+                    kbr.ReadBytes(4); // 总是DF FF FF FF
+                    if (!dict.ContainsKey(key))
+                    {
+                        dict[key] = new Loc();
+                    }
+                    // 字符数量 ^ 0xFFFFFFFF，* 2后为字节数
+                    var value = Encoding.Unicode.GetString(vbr.ReadBytes(-2 - (vbr.ReadInt32() * 2))) + $" #{str},{key},{unknown}";
+                    vbr.ReadInt16(); // 总是0
+                    switch (lang)
+                    {
+                        case "en":
+                            dict[key].English = value;
+                            break;
+                        case "zh-Hans":
+                            dict[key].Chinese = value;
+                            break;
+                    }
+                }
+                */
+                #endregion
             }
-            File.WriteAllText(args[1], JsonConvert.SerializeObject(dict, Formatting.Indented));
+            File.WriteAllText(@"E:\Projects\dbdt\data.json", JsonConvert.SerializeObject(dict, Formatting.Indented));
         }
     }
 }
